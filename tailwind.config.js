1fca6ea8a909cabe0ecbd937f01502f9c751c8ce
enrihg <99612570+enrihg@users.noreply.github.com>
2024-03-08 14:31:03 -0300

    header added

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'medium-brown' : '#71746e',
+      'light-cream' : '#fff7f0',
+      'galactic-blue' : '#755cde',
+      'summer-yellow' : '#f6a560',
+      'pink' : '#f39e9e',
+      'light-red' : '#eb7565',
+      'cyan' : '#61c4b7',
+      'dark-purple': "#552049",
+      'black' : '#030303',
+    },
+
+  },
+  plugins: [],
+}
+
